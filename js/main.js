1081e01faff73b7088b237c3543d73ae4299e100
lozzabing <laurenbingham85@gmail.com>
2023-06-28 19:50:18 +1000

    Commented out unnecessary JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,79 @@
-console.log ("hi");
-//alert ("hey");
-function codingBoss () {
-    document.querySelector ('#hand').style.backgroundColor = "hotpink";
-}
-codingBoss ();
+const companyName = "Lauren Bingham VA";
+let vaccinated = "true";
+
+// console.log(morning);
+// console.log(firstPrize);
+// console.clear();
+
+let firstName = "Pippi";
+
+console.log("Your pup's name is " + firstName);
+console.log(`Your cat's name is ${firstName}`);
+
+let welcomeSentence = `Hey there thanks for stopping by ${firstName}`;
+
+let result = welcomeSentence.toUpperCase();
+
+console.log(result);
+
+console.log(`${result.length}`);
+
+let resultLength = result.length;
+console.log(resultLength);
+
+const sentence = "Baby hot baby OK";
+console.log(sentence.toLowerCase());
+
+const word = "Pets";
+console.log(word.toUpperCase());
+
+console.log(word);
+
+console.log(word + 599);
+
+console.error("Error!");
+
+console.warn("Warning!");
+
+console.log(30+10);
+
+// let Cali = 5
+// let Archie = 3
+// let Pippi = 9
+// console.log(Cali >=1 && Cali <=4);
+// console.log(Archie >=1 && Archie <=4);
+// console.log(Pippi >=12 || Cali >=12);
+// console.log(Pippi == 9);
+// console.log(Cali != 5);
+
+// let age = 0.5;
+
+// const puppySchool = (age < 1) ? "You're Enrolled!" : "Check out our adult doggy classes!";
+
+// console.log(puppySchool);
+
+// console.log(typeof puppySchool);
+
+// let petAge = 1;
+// console.log(petAge++);
+// console.log(petAge++);
+// console.log(petAge++);
+// console.log(petAge--);
+// console.log(petAge--);
+
+// if (petAge >= 5) {
+//     greeting();
+// } else {
+//     greetingWelcome();
+// }
+
+// function greetingWelcome () {
+//     alert('Welcome!');
+// }
+
+// function greeting () {
+//     alert('Turn back!');
+// }
+// console.log(petAge);
+
+
